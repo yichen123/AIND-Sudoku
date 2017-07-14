7d1396f60aebd14c153ea0ab2897bf2e9455234e
yichen123 <chenyi330@hotmail.com>
2017-07-14 16:36:39 +0800

    Done

--- README.md
+++ README.md
@@ -3,17 +3,19 @@
 
 # Question 1 (Naked Twins)
 Q: How do we use constraint propagation to solve the naked twins problem?  
-A: *Student should provide answer here*
+A: go through all the units, and for each unit, check if there is any boxes with the same candidates, and check if the number of such boxes equal the number of candidates. If true, then we found a naked twins and eliminate all possibilities of such candidates in other boxes of this unit.
+
+Naked Twins technique will be used in the reduce_puzzle step after elimination and only_choice, which can reduce our searching space, and reduce the computation cost.
 
 # Question 2 (Diagonal Sudoku)
 Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
-A: *Student should provide answer here*
+A: We just add a new diagonal units into the unit lists, and our system will do the rest jobs.
 
 ### Install
 
 This project requires **Python 3**.
 
-We recommend students install [Anaconda](https://www.continuum.io/downloads), a pre-packaged Python distribution that contains all of the necessary libraries and software for this project. 
+We recommend students install [Anaconda](https://www.continuum.io/downloads), a pre-packaged Python distribution that contains all of the necessary libraries and software for this project.
 Please try using the environment we provided in the Anaconda lesson of the Nanodegree.
 
 ##### Optional: Pygame
@@ -41,4 +43,3 @@ The setup is simple.  If you have not installed the client tool already, then yo
 To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
 
 This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
-
--- solution.py
+++ solution.py
@@ -1,5 +1,22 @@
 assignments = []
 
+### puzzle board Set-ups
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
+cols = '123456789'
+rows = 'ABCDEFGHI'
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diagonal_units = [['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9'], ['A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1']]
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -26,10 +43,21 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
+    for unit in unitlist:
+        for box in unit:
+            repeats = [box2 for box2 in unit if values[box] == values[box2]]
+            if len(repeats) > 1 and len(repeats) == len(values[box]):
+                del_box = [box2 for box2 in unit if values[box] != values[box2]]
+                for deleting in del_box:
+                    for digit in values[box]:
+                        if digit in values[deleting]:
+                            assign_value(values, deleting, values[deleting].replace(digit, ''))
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    return values
+
+
+
+
 
 def grid_values(grid):
     """
@@ -41,27 +69,119 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    values = []
+    for char in grid:
+        if char == '.':
+            values.append('123456789')
+        else:
+            values.append(char)
+    return dict(zip(boxes, values))
+
 
 def display(values):
     """
     Display the values as a 2-D grid.
     Args:
         values(dict): The sudoku in dictionary form
+    display code is copied from the lecture notes
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    """
+    Deleting candidates that has been confirmed in other boxes
+    Args:
+        values: Sudoku board
+    """
+    changed = True
+    while changed:
+
+        changed = False
+        for box in boxes:
+            if len(values[box]) == 1:
+                digit = values[box]
+                for peer in peers[box]:
+                    if digit in values[peer]:
+                        assign_value(values, peer, values[peer].replace(digit, ''))
+                        changed = True
+    return values
+
 
 def only_choice(values):
-    pass
+    """
+    confirm digits that not a candidates in other boxes
+    Args:
+        values: sudoku board
+    """
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                assign_value(values, dplaces[0], digit)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    iterate through eliminate, only_choice, and naked_twins techniques until no changed can be done.
+    Args:
+        values: sudoku board
+    Returns:
+        False: if contradiction appears
+        values: reduce puzzle board
+
+    """
+
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    changed = True
+    while changed:
+        changed = False
+        while not stalled:
+            solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+            values = eliminate(values)
+
+            values = only_choice(values)
+            solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+            stalled = solved_values_before == solved_values_after
+            if len([box for box in values.keys() if len(values[box]) == 0]):
+                return False
+            values2 = naked_twins(values)
+            if values != values2:
+                values = values2
+                changed = True
+    return values
 
 def search(values):
-    pass
+    """
+    Perform a recurrence depth first search on a reduced puzzle
+    Args:
+        values: sudoku board
+    Returns:
+        False: failed attempts
+        values: solved puzzle board
+
+    """
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and
+    for value in values[s]:
+        new_sudoku = values.copy()
+        assign_value(new_sudoku, s, value)
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -72,6 +192,15 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = grid_values(grid)
+
+    values = search(values)
+
+
+
+    return values
+
+
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
